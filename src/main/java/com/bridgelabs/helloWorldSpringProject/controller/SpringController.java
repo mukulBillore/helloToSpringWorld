b222e6e888234450c378f5adb2a22ae1a21955d2
mukulBillore <mukulbillore88@gmail.com>
2022-02-17 09:23:47 +0530

    Added : added the restCall to show using post request body

--- src/main/java/com/bridgelabs/helloWorldSpringProject/controller/SpringController.java
+++ src/main/java/com/bridgelabs/helloWorldSpringProject/controller/SpringController.java
@@ -2,6 +2,8 @@ package com.bridgelabs.helloWorldSpringProject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,10 @@ public class SpringController {
 	public String sayHelloByParam(@RequestParam String name)
 	{
 		return "hello  "+name+"  from bridge labs";
+	}
+	@PostMapping("/user")
+	public String sayHelloByParam(@RequestBody User user)
+	{
+		return "hello "+user.getFirstName()+" "+user.getLastName()+"from bridgelabs";
 	}
 }
