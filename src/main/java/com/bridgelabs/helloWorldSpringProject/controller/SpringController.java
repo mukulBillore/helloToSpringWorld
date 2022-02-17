c4eab6401c8eeba4f5e5245f11041d8659832b93
mukulBillore <mukulbillore88@gmail.com>
2022-02-17 08:32:52 +0530

    Added : added the messege with getMapping

--- src/main/java/com/bridgelabs/helloWorldSpringProject/controller/SpringController.java
+++ src/main/java/com/bridgelabs/helloWorldSpringProject/controller/SpringController.java
@@ -7,19 +7,12 @@ import com.bridgelabs.helloWorldSpringProject.model.User;
 
 @RestController
 public class SpringController {
-	
-@GetMapping("/homePage")
-public String sayHello() {
-	System.out.println("This is the console output for homePage");
-	return "hello Spring";
-}
-@GetMapping("/")
-public String homePage() {
-	return "This is the home page of site";
-}
 
-@GetMapping("/User")
-public User userRegistration(User user) {
- return user;	
-}
+	@GetMapping("/")
+	public String sayHello() {
+		return "hello from bridgelabs";
+	}
+
+	
+
 }
