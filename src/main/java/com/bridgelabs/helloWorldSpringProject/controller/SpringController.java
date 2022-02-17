24d56d56655ba1769f38d849b6eedabfa401cbda
mukulBillore <mukulbillore88@gmail.com>
2022-02-17 08:57:24 +0530

    Added : added the messege with getting  name by path variable

--- src/main/java/com/bridgelabs/helloWorldSpringProject/controller/SpringController.java
+++ src/main/java/com/bridgelabs/helloWorldSpringProject/controller/SpringController.java
@@ -1,6 +1,8 @@
 package com.bridgelabs.helloWorldSpringProject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabs.helloWorldSpringProject.model.User;
@@ -13,6 +15,9 @@ public class SpringController {
 		return "hello from bridgelabs";
 	}
 
-	
+	@GetMapping("/query/{name}")
+	public String sayHelloByParam(@PathVariable String name ) {
+	return "hello  "+name+"  from bridge labs";
+	}
 
 }
